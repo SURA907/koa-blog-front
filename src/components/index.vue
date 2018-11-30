9542d86971853209772ae09372cc186a48dd063c
sura <sura907@hotmail.com>
2018-11-30 15:41:55 +0800

    remove some code, rewrite

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,30 +1,23 @@
 <template>
-  <div v-if="isIE">
-    <h1>检测到您使用了ie浏览器</h1>
-    <h2>为保证浏览效果，请使用chrome、firefox、opera等现代浏览器</h2>
-  </div>
-  <div v-else>
-    <h1>浏览器检测通过</h1>
+  <div>
+    <button @click="btn_click">Index模块</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'index',
-  data() {
-    return {
-      isIE: false
-    }
-  },
-  mounted() {
-    // 浏览器检测
-    if ('ActiveXObject' in window) {
-      this.isIE = true
-    }
+  name: 'Index',
+  methods: {
+    btn_click() {
+
+    },
   }
 }
 </script>
 
 <style scoped>
-
+  button{
+    cursor: pointer;
+  }
 </style>
+
